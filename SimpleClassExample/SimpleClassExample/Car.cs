0ffab72a988e66e9f3e8acfb429f1b53bae24f34
Олесуа и Юля <astahova.98@gmail.com>
2018-02-02 10:18:12 +0700

    Класс Car

--- SimpleClassExample/SimpleClassExample/Car.cs
+++ SimpleClassExample/SimpleClassExample/Car.cs
@@ -12,6 +12,24 @@ namespace SimpleClassExample
         public string petName;
         public int currSpeed;
 
+        // Специальный стандартный конструктор.
+        public Car()
+        {
+            petName = "Chuck";
+            currSpeed = 10;
+        }
+
+        //Стандартное значение для типа int (0)
+        public Car(string pn)
+        {
+            petName = pn;
+        }
+        // Установка кодом полного состояния Car.
+        public Car(string pn, int cs)
+        {
+            petName = pn;
+            currSpeed = cs;
+        }
         // Функциональность Car.
         public void PrintState()
         {
--- SimpleClassExample/SimpleClassExample/Program.cs
+++ SimpleClassExample/SimpleClassExample/Program.cs
@@ -10,12 +10,21 @@ namespace SimpleClassExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***** Fun with Class Types *****\n");
+            // Console.WriteLine("***** Fun with Class Types *****\n");
             //Разместить в памяти и сконфигурировать объект Car.
-            Car myCar = new Car();
-            myCar.petName = "Henry";
-            myCar.currSpeed = 10;
-
+            //Car myCar = new Car();
+            //myCar.petName = "Henry";
+            //myCar.currSpeed = 10;
+            Console.WriteLine("***** Fun with Class Types *****\n");
+            Car chuck = new Car();
+            chuck.PrintState();
+            Car mary = new Car("Mary");
+            mary.PrintState();
+            Car daisy = new Car("Daisy", 75);
+            daisy.PrintState();
+            Car myCar;
+            myCar = new Car();
+            myCar.petName = "Fred";
             // Увиличить скорость автомобиля в несколько раз и вывести новое состояние.
             for (int i = 0; i <= 10; i++)
             {
