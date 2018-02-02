5fdfe6b66564383a397fecd6fb77b811c6cfe8ac
Олесуа и Юля <astahova.98@gmail.com>
2018-02-02 09:56:20 +0700

    Модификация кода класса и обновление кода метода Main()

--- SimpleClassExample/SimpleClassExample/Car.cs
+++ SimpleClassExample/SimpleClassExample/Car.cs
@@ -8,5 +8,19 @@ namespace SimpleClassExample
 {
     class Car
     {
+        //'Состояние ' объект Car.
+        public string petName;
+        public int currSpeed;
+
+        // Функциональность Car.
+        public void PrintState()
+        {
+            Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+        }
+
+        public void SpeedUp(int delta)
+        {
+            currSpeed += delta;
+        }
     }
 }
--- SimpleClassExample/SimpleClassExample/Program.cs
+++ SimpleClassExample/SimpleClassExample/Program.cs
@@ -10,6 +10,20 @@ namespace SimpleClassExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Class Types *****\n");
+            //Разместить в памяти и сконфигурировать объект Car.
+            Car myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+
+            // Увиличить скорость автомобиля в несколько раз и вывести новое состояние.
+            for (int i = 0; i <= 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+            Console.ReadLine();
         }
     }
+    
 }
